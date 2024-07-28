dd9c9fbefe993c3082bdb5fc07bd9543340a90b4
bugbounty88 <156437134+bugbounty88@users.noreply.github.com>
2024-07-28 04:26:46 +0300

    Update RPstring.h

--- RPstring.h
+++ RPstring.h
@@ -1,1 +1,15 @@
-#include
+#include <iostream>
+using namespace std;
+
+string repeateStr(string str, int r){
+	string result;
+	for (int i = 0; i < r; i++){
+		result += str;
+	}
+	return result;
+}
+
+int main(){
+	cout << repeateStr("Hamza \n", 10000000) << endl;
+	return 0;
+}
